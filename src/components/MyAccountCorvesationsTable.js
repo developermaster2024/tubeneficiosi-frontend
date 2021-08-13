dee64f04e129430f63cc82167d747378eafd515b
Jeyver Vegas <tridant16@gmail.com>
2021-08-13 08:28:08 -0400

    fix (Questions): fix questions of the user

--- src/components/MyAccountCorvesationsTable.js
+++ src/components/MyAccountCorvesationsTable.js
@@ -36,16 +36,16 @@ const MyAccountConversationsTable = (props) => {
         <div className="w-1/12 p-2">
           <button className="bg-main px-8 py-2 text-white font-bold">
             Buscar
-            </button>
+          </button>
         </div>
       </div>
       <div className="w-full text-center">
         {
-          conversations.map((conversation, i) => <div className="flex bg-white my-4 p-6 items-center rounded-lg transition duration-300 transform hover:shadow-xl hover:-translate-y-2">
+          conversations.map((conversation, i) => <div key={i} className="flex bg-white my-4 p-6 items-center rounded-lg transition duration-300 transform hover:shadow-xl hover:-translate-y-2">
             <div className="w-2/12">
               {
-                conversation.store ?
-                  <img className="w-[50px] m-auto" src={conversation.store.img} alt="" />
+                conversation?.store ?
+                  <img className="w-[50px] m-auto" src={conversation?.store?.img} alt="" />
                   :
                   <IoImage className="m-auto text-[50px]"></IoImage>
               }
@@ -53,24 +53,24 @@ const MyAccountConversationsTable = (props) => {
             <div className="w-3/12">
               {
                 conversation.store ?
-                  <Link to={`/stores/${conversation.store.slug}`} className="text-blue-500">
-                    {conversation.store.name}
+                  <Link to={`/stores/${conversation?.store?.slug}`} className="text-blue-500">
+                    {conversation?.store?.name}
                   </Link>
                   :
                   'No tiene tienda asociada'
               }
             </div>
             <div className="w-3/12">
-              {conversation.subject}
+              {conversation?.subject}
             </div>
             <div className={clsx(["w-3/12 font-bold bg-opacity-10"], {
-              'text-green-500': conversation.status.id === 2,
-              'text-red-500': conversation.status.id === 1
+              'text-green-500': conversation?.status?.id === 2,
+              'text-red-500': conversation?.status?.id === 1
             })}>
-              {conversation.status.title}
+              {conversation?.status?.title}
             </div>
             <div className="w-1/12">
-              <Link to={`/my-account/conversations/${conversation.id}`} className="hover:text-main transition duration-300">
+              <Link to={`/my-account/conversations/${conversation?.id}`} className="hover:text-main transition duration-300">
                 Abrir chat
                 <IoArrowForwardOutline className="m-auto text-2xl"></IoArrowForwardOutline>
               </Link>
--- src/pages/my-account/Conversations.js
+++ src/pages/my-account/Conversations.js
@@ -1,20 +1,74 @@
-import MyAccountConversationsTable from '../../components/MyAccountCorvesationsTable';
-import { conversations } from '../../util/conversations';
-import { IoChatboxEllipsesOutline } from "react-icons/io5";
-import Pagination from '../../components/Pagination';
-import { useEffect, useState } from 'react';
-import useQuestions from '../../hooks/useQuestions';
+import clsx from "clsx";
+import { useState } from "react";
+import { IoChatboxEllipsesOutline, IoChevronDownSharp, IoChevronUp, IoArrowRedoSharp, IoCheckmarkCircleSharp } from "react-icons/io5";
+import { useEffect } from "react";
+import { useAuth } from "../../contexts/AuthContext";
+import useQuestions from "../../hooks/useQuestions";
+import Pagination from "../../components/Pagination";
 
+
+const tabsLink = [
+  {
+    value: 1,
+    title: 'Preguntas',
+    icon: <IoChatboxEllipsesOutline />
+  }
+]
+
+const TabBody = ({ children, show, className }) => {
+  return (
+    <div hidden={!show} className={`${className} p-4 animate__animated animate__fadeIn`}>
+      {children}
+    </div>
+  )
+}
 
 const MyAccountConversations = () => {
 
-  const [filters, setFilters] = useState({ storeName: "", productName: "", question: "", page: 1 });
+  const { setLoading, setCustomAlert, user } = useAuth();
 
-  const [{ questions, error: questionsError, loading: questionsLoading, total, numberOfPages }] = useQuestions({ axiosConfig: { params: { ...filters } } });
+  const [selectedQuestion, setSelectedQuestion] = useState(null);
+
+  const [show, setShow] = useState(false);
+
+  const [showAnswer, setShowAnswer] = useState(null);
+
+  const [activeTab, setActiveTab] = useState(1);
+  const [filters, setFilters] = useState({
+    page: 1,
+    sort: "createdAt,DESC",
+    askedById: user?.id
+  });
+
+  const [{ questions, numberOfPages, error, loading }, getQuestions] = useQuestions({
+    axiosConfig: {
+      params: {
+        ...filters
+      }
+    }
+  });
+
+  useEffect(() => {
+    if (error) {
+      setLoading?.({ show: false, message: "" });
+      setCustomAlert?.({ show: true, message: `Ha ocurrido un error: ${error?.response?.status === 400 ? error?.response?.data.message[0] : error?.response?.data.message}.`, severity: "error" });
+    }
+  }, [error]);
 
   useEffect(() => {
     console.log(questions);
   }, [questions])
+
+  useEffect(() => {
+    setLoading({ show: loading, message: "Cargando preguntas" })
+  }, [loading])
+
+
+  const handleShowAnswer = (i) => {
+    setShowAnswer((oldShowAnswer) => {
+      return oldShowAnswer === i ? null : i
+    });
+  }
 
   const handleChange = (e) => {
     setFilters((oldFilters) => {
@@ -26,22 +80,92 @@ const MyAccountConversations = () => {
   }
 
   return (
-    <div className="px-8">
-      <h1 className="text-2xl flex items-center text-gray-600 font-bold my-5">
-        <IoChatboxEllipsesOutline className="text-4xl"></IoChatboxEllipsesOutline>
-        <span className="ml-4">Preguntas</span>
-      </h1>
+    <div className="p-4">
+      <div className="flex items-center text-3xl text-gray-500 mb-8">
+        <IoChatboxEllipsesOutline />
+        <p>Preguntas y respuestas</p>
+      </div>
+      <div className="bg-white rounded">
+        <div>
+          <div className="flex items-center justify-between">
+            {
+              tabsLink.map((tab, i) => {
+                return (
+                  <div className={clsx([`flex justify-center w-full text-xl text-gray-500 items-center cursor-pointer p-2`],
+                    {
+                      'text-main border-b border-main': tab.value === activeTab
+                    }
+                  )} key={i} onClick={() => { setActiveTab(tab.value) }}>
+                    {tab.icon}
+                    <h1 className="ml-2">{tab.title}</h1>
+                  </div>
+                )
+              })
+            }
+          </div>
+          <div>
+            <TabBody show={activeTab === 1}>
+              {
+                questions.map((question, i) => {
+                  return (
+                    <div key={i} className="border-b py-4">
+                      {
+                        question?.answer &&
+                        <div className="flex justify-end items-center text-xl text-green-500">
+                          Respondida < IoCheckmarkCircleSharp />
+                        </div>
+                      }
+                      <div className="flex items-center">
+                        <div className="w-1/2">
+                          <div className="flex items-center">
+                            <img className="w-1/12 rounded-full" src={`${process.env.REACT_APP_API_URL}/${question?.product?.productImages?.[0].path}`} alt="" />
+                            <a href={`${process.env.REACT_APP_HOST}products/${question?.product?.slug}`} className="text-blue-500 text-xl">
+                              {question?.product?.name}
+                            </a>
+                          </div>
+                          <div className="text-gray-500 mt-4">
+                            {question?.question ? question?.question : <span className="text-red-500">No hay pregunta.</span>} - <b>{question.createdAt.toLocaleString()}</b>
+                          </div>
+                        </div>
+                        <div className="w-1/2">
+                          <div className="font-bold text-gray-500">
+                            Tienda:
+                          </div>
+                          <div className="text-right">
+                            <img className="ml-auto rounded-full w-16 h-16 shadow-lg" src={`${process.env.REACT_APP_API_URL}/${question?.product?.store?.storeProfile?.logo}`} alt="" />
+                            <a target="_blank" className="text-blue-500" href={`${process.env.REACT_APP_HOST}stores/${question?.product?.store?.slug}`}>{question?.product?.store?.name}</a>
+                          </div>
+                        </div>
+                      </div>
+                      {
+                        question?.answer &&
+                        <div className="text-right">
+                          <button className="flex items-center ml-auto text-main" onClick={() => { handleShowAnswer(i) }}>
+                            Ver respuesta {showAnswer === i ? <IoChevronUp /> : <IoChevronDownSharp className="ml-2" />}
+                          </button>
+                          {
+                            showAnswer === i &&
+                            <div className="animate__animated animate__fadeInLeft animate__faster text-left text-gray-500">
+                              {question?.answer} - <b>{question?.answeredAt.toLocaleString()}</b>
+                            </div>
+                          }
+                        </div>
 
-      <MyAccountConversationsTable conversations={conversations} className="w-full my-12 text-gray-500 text-center" />
-
-      {
-        numberOfPages > 0 &&
-        <Pagination
-          pages={numberOfPages}
-          activePage={filters.page}
-          onChange={e => { handleChange({ target: { name: "page", value: e } }) }}
-        />
-      }
+                      }
+                    </div>
+                  )
+                })
+              }
+            </TabBody>
+          </div>
+        </div>
+        {
+          numberOfPages > 1 &&
+          <div className="py-4">
+            <Pagination pages={numberOfPages} activePage={filters.page} onChange={e => { handleChange({ target: { name: "page", value: e } }) }} />
+          </div>
+        }
+      </div>
     </div>
   )
 }
